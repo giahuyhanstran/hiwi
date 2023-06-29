4b235298e274f44090fd8c1fdaeff5c5d540fb33
Tony <tonyholz80@gmail.com>
2023-06-29 13:53:52 +0200

    add mqtt compatibility

--- camera/process_video.py
+++ camera/process_video.py
@@ -1,71 +1,83 @@
 # TODO concate Video
 import os
 import pickle
-import re
 import cv2
-import subprocess
 import pyaudio
 import wave
+import paho.mqtt.client as mqtt
+import datetime
+import keyboard
+from moviepy.editor import VideoFileClip, AudioFileClip
 
-FRAME_WIDTH = 640
-FRAME_HEIGHT = 480
-FPS = 30
-DIRECTORY = '2023-06-28_08-50-30'
-merged_data = {}
-vw = cv2.VideoWriter(f'{DIRECTORY}_recreated_video.mp4', cv2.VideoWriter_fourcc(*"mp4v"), FPS,
-                     (FRAME_WIDTH, FRAME_HEIGHT))
-
-sorted_pickles = sorted(os.listdir(DIRECTORY), key=lambda x: (
-    int(re.match(r'^(\d+)', x).group(0)) if re.match(r'^(\d+)', x) else float('inf'), x))
-
-
-def unpickle(frame_directory):
-    frame_counter = 1
-    for filename in sorted_pickles:
-        if filename.endswith('.pickle'):
-            frame_counter += 1
-            file_path = os.path.join(frame_directory, filename)
-            with open(file_path, 'rb') as file:
-                content = pickle.load(file)
-                merged_data[frame_counter] = content
+'''
+Note: Script is listening on localhost port 1883 for topic "Camera"
+      At first start HiveMQ Broker, then run record_camera.py, then
+      run this script. You can use MQTT Explorer to check for data traffic.
+'''
 
 
-def combine_video(merged_data, video_writer):
-    for video in merged_data:
-        video_writer.write(merged_data[video][0])
-    video_writer.release()
+def on_message(client, userdata, message):
+    global vw
+    global wf
 
-
-audio_sample_rate = 44100
-audio_chunk_size = int(audio_sample_rate / FPS)
-audio_format = pyaudio.paInt16
-audio_channels = 2
-
-
-def combine_audio_frames(merged_data):
-    audio_frames = []
-    for audio in merged_data:
-        audio_frames.append(merged_data[audio][1])
-    audio_frames = audio_frames[:len(audio_frames) // FPS * FPS]
-    print(len(audio_frames))
-    wf = wave.open(f'{DIRECTORY}_recreated_audio.wav', 'wb')
-    wf.setnchannels(audio_channels)
-    wf.setsampwidth(pyaudio.PyAudio().get_sample_size(audio_format))
-    wf.setframerate(audio_sample_rate)
-    wf.writeframes(b''.join(audio_frames))
-    wf.close()
-    pass
+    payload = message.payload
+    content = pickle.loads(payload)
+    vw.write(content[0])
+    wf.writeframes(content[1])
 
 
 def merge_audio_video(output_file):
-    video_file = os.path.join(os.getcwd(), f'{DIRECTORY}_recreated_video.mp4')
-    audio_file = os.path.join(os.getcwd(), f'{DIRECTORY}_recreated_audio.wav')
+    video = VideoFileClip(f'{current_date}.mp4')
+    audio = AudioFileClip(f'{current_date}.wav')
+    final_video = video.set_audio(audio)
+    final_video.write_videofile(output_file, codec="libx264", audio_codec="aac")
 
-    command = f'ffmpeg -i "{video_file}" -i "{audio_file}" -c:v copy -c:a aac -strict experimental "{output_file}"'
-    subprocess.call(command, shell=True)
 
+if __name__ == '__main__':
 
-unpickle(DIRECTORY)
-combine_video(merged_data, vw)
-combine_audio_frames(merged_data)
-merge_audio_video('merged_output.mp4')
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    DIRECTORY = os.path.join(os.getcwd(), current_date)
+    if not os.path.exists(DIRECTORY):
+        os.makedirs(DIRECTORY)
+    os.chdir(DIRECTORY)
+
+    FRAME_WIDTH = 640
+    FRAME_HEIGHT = 480
+    FPS = 30
+
+    merged_data = {}
+    frame_counter: int = 0
+
+    audio_sample_rate = 44100
+    audio_chunk_size = int(audio_sample_rate / FPS)
+    audio_format = pyaudio.paInt16
+    audio_channels = 2
+
+    vw = cv2.VideoWriter(f'{current_date}.mp4', cv2.VideoWriter_fourcc(*"mp4v"), FPS, (FRAME_WIDTH, FRAME_HEIGHT))
+    wf = wave.open(f'{current_date}.wav', 'wb')
+    wf.setnchannels(audio_channels)
+    wf.setsampwidth(pyaudio.PyAudio().get_sample_size(audio_format))
+    wf.setframerate(audio_sample_rate)
+
+    # mqtt
+    mqttBroker = "localhost"
+    client = mqtt.Client("Laptop")
+    client.connect(host=mqttBroker, port=1883)
+
+    client.loop_start()
+    client.subscribe("Camera")
+    client.on_message = on_message
+    # Loop until the stop key is pressed
+    while True:
+        # Check for a key press
+        key = cv2.waitKey(1) & 0xFF
+
+        # Break the loop if the stop key is pressed
+        if keyboard.is_pressed('q'):
+            break
+
+    client.loop_stop()
+
+    vw.release()
+    wf.close()
+    merge_audio_video(f'{current_date}_merged.mp4')
--- camera/record_camera.py
+++ camera/record_camera.py
@@ -1,27 +1,18 @@
-import json
-import shutil
-import subprocess
 import cv2
 import pyaudio
-import wave
-import datetime
-import os
 import pickle
+import paho.mqtt.client as mqtt
 
+'''
+Note: Script is publishing on localhost port 1883 for topic "Camera"
+      At first start HiveMQ Broker, then run this script, then
+      run process_video.py. You can use MQTT Explorer to check for data traffic.
+'''
 
-# TODO optional argparse parameters from b4
-def record_video_audio(video_output_file, audio_output_file, current_date, dir_path):
-    # Create a folder to stor files
-    os.makedirs(current_date)
-
-    video_output_file = os.path.join(current_date, video_output_file)
-    audio_output_file = os.path.join(current_date, audio_output_file)
-
+def record_video_audio():
     cap = cv2.VideoCapture(0)
 
     # default settings
-    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     audio_format = pyaudio.paInt16
     audio_channels = 2
@@ -35,95 +26,37 @@ def record_video_audio(video_output_file, audio_output_file, current_date, dir_p
                     input=True,
                     frames_per_buffer=audio_chunk_size)
 
-    video_writer = cv2.VideoWriter(video_output_file, cv2.VideoWriter_fourcc(*"mp4v"), fps, (frame_width, frame_height))
-    audio_frames = []
-    current_frame = 0
-
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
 
-        current_frame += 1
-        file_path = os.path.join(dir_path, f"{current_frame}.pickle")
-
-        video_writer.write(frame)
-
         audio_data = stream.read(audio_chunk_size)
-        audio_frames.append(audio_data)
-
-        # rm later
         video_data = (frame, audio_data)
-        with open(file_path, 'wb') as file:
-            pickle.dump(video_data, file)
 
-        # TODO MQTT
-        ### MQTT
+        # Publish video_data as an MQTT message
+        client.publish("Camera", pickle.dumps(video_data))
 
-
-        temp_data = pickle.dumps(video_data)
-
-        ###
         cv2.imshow("Camera", frame)
 
-        # break con
+        # break condition
         if cv2.waitKey(1) == ord('q'):
             break
 
-    # cleanup vid
-    video_writer.release()
+    # cleanup
     cap.release()
     cv2.destroyAllWindows()
-
-    print(len(audio_frames))
-    audio_frames = audio_frames[:len(audio_frames) // fps * fps]
-    print(len(audio_frames))
-
-    # Save audio frames in audio file
-    wf = wave.open(audio_output_file, 'wb')
-    wf.setnchannels(audio_channels)
-    wf.setsampwidth(p.get_sample_size(audio_format))
-    print(p.get_sample_size(audio_format))
-    wf.setframerate(audio_sample_rate)
-    wf.writeframes(b''.join(audio_frames))
-    wf.close()
 
     # clean up audio
     stream.stop_stream()
     stream.close()
     p.terminate()
 
-    # print(audio_frames)
+if __name__ == '__main__':
 
-
-# ffmpeg command to merge audio and vid
-def merge_audio_video(output_file, current_date):
-    video_file = os.path.join(current_date, 'output_video.mp4')
-    audio_file = os.path.join(current_date, 'output_audio.wav')
-
-    command = f'ffmpeg -i "{video_file}" -i "{audio_file}" -c:v copy -c:a aac -strict experimental "{output_file}"'
-    subprocess.call(command, shell=True)
-
-
-current_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-
-# save everything in folder
-video_output_file = 'output_video.mp4'
-audio_output_file = 'output_audio.wav'
-
-audio_file_path = os.path.join(current_date, audio_output_file)
-directory_path = os.path.join(os.getcwd(), current_date)
-
-record_video_audio(video_output_file, audio_output_file, current_date, directory_path)
-merge_audio_video('merged_output.mp4', current_date)
-
-file_path_merged = os.path.join(os.getcwd(), "merged_output.mp4")
-
-if not os.path.exists(directory_path):
-    os.makedirs(directory_path)
-
-shutil.move(file_path_merged, directory_path)
-
-# TODO Clean up code
-# TODO impl. partition into frames w/  ffmpeg
-# TODO MQTT to sync up 4 later
+    # Define MQTT Client and address of broker
+    mqttBroker = "localhost"
+    client = mqtt.Client("camera data")
+    client.connect(host=mqttBroker, port=1883)
+    
+    record_video_audio()
