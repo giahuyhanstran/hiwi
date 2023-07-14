2eebffa348618be6dfc2061ee4c5cb356f502385
Tony Holz <tony.holz@uni-rostock.de>
2023-07-14 07:30:43 +0200

    add comments, use choices

--- data_recording/record_pixel_data.py
+++ data_recording/record_pixel_data.py
@@ -25,18 +25,34 @@ def _get_config(filename='config.yml') -> dict:
 
     return cfg
 
+def fill_choices(cfg):
+    sensor_mac = []
+    names = []
+    for zone, zone_info in cfg.items():
+        if "ZONE" in zone and "MAC_ADDRESS" in zone_info:
+            sensor_mac.append(zone_info["MAC_ADDRESS"])
+            if "NAME" in zone_info:
+                names.append(zone_info["NAME"])
+    return sensor_mac + names
 
 def main():
     
-    parser = argparse.ArgumentParser() #TODO Erklärung
-    parser.add_argument('--include', nargs='+', default=[]) #TODO default auf None setzen
-    parser.add_argument('--exclude', nargs='+', default=[])
-    parser.add_argument('--type')
+    cfg = _get_config()
+    choices = fill_choices(cfg)
+
+    # Get all choices for include and exclude arguments
+    parser = argparse.ArgumentParser(description= "include: Get only that data, exclude: Get everything else, type: Select the data type of interest, default: Don't pass args and get everything.")
+    parser.add_argument('--include', type=str, nargs='+', choices=choices, default=None, help='Of what sensor units you do want to receive data. Choose between Mac or Name.')
+    parser.add_argument('--exclude', type=str, nargs='+', choices=choices, default=None, help='Of what sensor units you do not want to receive data. Choose between Mac or Name.')
+    parser.add_argument('--type', type=str, choices=['tof', 'thermal'], help='What kind of data you want to receive of the selected sensor units.')
     args = parser.parse_args()
 
-    cfg = _get_config()
-    receiver = MQTTSubscriber(cfg, args)
-    receiver.receive_messages()
+    print(args.include)
+    print(args.exclude)
+    print(args.type)
+
+    # receiver = MQTTSubscriber(cfg, args)
+    # receiver.receive_messages()
 
 if __name__ == '__main__':
     main()
