ea7b224353c78533c4f59adb2a866295d04807f7
Tony Holz <tony.holz@uni-rostock.de>
2023-07-16 19:26:19 +0200

    add 2 temp test files

---
+++ camera/test2.py
@@ -0,0 +1,34 @@
+import paho.mqtt.client as mqtt
+import time
+import uuid
+import struct
+
+mqttBroker = "localhost"
+client = mqtt.Client("Topi")
+client.connect(host = mqttBroker, port = 1883)
+uuid_bytes1 = uuid.UUID(str(uuid.uuid4())).bytes
+uuid_bytes2 = uuid.UUID(str(uuid.uuid4())).bytes
+frame_counter1 = 0
+frame_counter2 = 1000
+
+topic1 = "heartbeat/device1"
+topic2 = "heartbeat/device2"
+
+
+while True:
+
+    frame_counter_bytes1 = struct.pack('!I', frame_counter1)
+    frame_counter_bytes2 = struct.pack('!I', frame_counter2)
+
+    data1 = bytearray(frame_counter_bytes1 + uuid_bytes1)
+    data2 = bytearray(frame_counter_bytes2 + uuid_bytes2)
+
+
+
+    client.publish(topic1, data1)
+    client.publish(topic2, data2)
+    print(f"Just pusblished {frame_counter1} to topic {topic1}")
+    print(f"Just pusblished {frame_counter2} to topic {topic2}")
+    frame_counter1 += 1
+    frame_counter2 += 1
+    time.sleep(1)
